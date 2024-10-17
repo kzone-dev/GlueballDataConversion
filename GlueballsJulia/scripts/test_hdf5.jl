e412a1c6e25cff1f734a7d5ce319ba4b2cb4a933
Fabian Zierler <fabian.zierler@a1.net>
2024-10-17 17:18:32 +0100

    fix reconstruction of gluonic contribution to correlation matrix from timeslices

--- GlueballsJulia/scripts/test_hdf5.jl
+++ GlueballsJulia/scripts/test_hdf5.jl
@@ -22,14 +22,21 @@ function reconstruct_corr(ops)
         for t2 in 1:T
             Δt = mod(t2-t1,T)
             for n in 1:Nmeas, op1 in 1:Nops, op2 in 1:Nops
-                corr[Δt+1,op1,op2,n] = ops[t1,op1,n]*ops[t2,op2,n]
+                corr[Δt+1,op1,op2,n] += (ops[t1,op1,n]*ops[t2,op2,n] + ops[t1,op2,n]*ops[t2,op1,n])/2
             end
         end
     end
     return @. corr
 end
-corr_reconstruct = reconstruct_corr(ops)
+function test_reconstructiuon(ops,corr0)
+    T, Nops, Nmeas = size(ops)
+    Tmax  = T÷2+1
+    corr1 = reconstruct_corr(ops)
+    maxdiff = maximum(abs.(corr0 - corr1[1:Tmax,:,:,:]))
+    return maxdiff
+end
 
-# TODO: Fix discrepancy by looking at Nuno's code
-corr[1,1,1,1]
-corr_reconstruct[1,1,1,1]
+# test reconstruction first
+diff = test_reconstructiuon(ops,corr)
+@assert iszero(diff)
+corr_reconstruct = reconstruct_corr(ops)
