8179d7313ad41c21c26c933653fa6741e767c84a
Fabian Zierler <fabian.zierler@a1.net>
2024-11-04 16:59:04 +0000

    test on production data

--- GlueballsJulia/scripts/reconstruct_gluon_block.jl
+++ GlueballsJulia/scripts/reconstruct_gluon_block.jl
@@ -1,24 +1,25 @@
 using Pkg; Pkg.activate("./GlueballsJulia")
 using GlueballsJulia
+using ProgressMeter
 using HDF5
 
-f = h5open("testfile.hdf5","r")["ensemble/0RPpR"]
+f = h5open("hdf5/glue_correlators.hdf5","r")["ensemble/0RPpR"]
 
 T = read(f,"NT")
 L = read(f,"NX")
 Nops  = read(f,"Nop")
 Nmeas = read(f,"Nmeas")
-corr  = read(f,"corr") # size is ( T/2+1, Nops, Nops, Nmeas)
+#corr  = read(f,"corr") # size is ( T/2+1, Nops, Nops, Nmeas)
 ops   = read(f,"ops")  # size is (T, Nops, Nmeas) 
 vev   = read(f,"vev")  # size is (Nops, Nmeas)
-@assert size(corr) == (T÷2+1, Nops, Nops, Nmeas)
+#@assert size(corr) == (T÷2+1, Nops, Nops, Nmeas)
 @assert size(ops)  == (T, Nops, Nmeas)
 @assert size(vev)  == (Nops, Nmeas)
 
 function reconstruct_corr(ops)
     T, Nops, Nmeas = size(ops)
     corr = zeros(T,Nops,Nops,Nmeas)
-    for t1 in 1:T
+    @showprogress for t1 in 1:T
         for t2 in 1:T
             Δt = mod(t2-t1,T)
             for n in 1:Nmeas, op1 in 1:Nops, op2 in 1:Nops
@@ -26,7 +27,7 @@ function reconstruct_corr(ops)
             end
         end
     end
-    return @. corr
+    return corr
 end
 function test_reconstructiuon(ops,corr0)
     T, Nops, Nmeas = size(ops)
@@ -37,6 +38,8 @@ function test_reconstructiuon(ops,corr0)
 end
 
 # test reconstruction first
-diff = test_reconstructiuon(ops,corr)
-@assert iszero(diff)
+# TODO: Fix issue with memory
 corr_reconstruct = reconstruct_corr(ops)
+#diff = test_reconstructiuon(ops,corr)
+#@assert iszero(diff)
+#corr_reconstruct = reconstruct_corr(ops)
