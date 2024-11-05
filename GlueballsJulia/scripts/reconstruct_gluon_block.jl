88f86749c7b90911293d9590f599a0884c093459
Fabian Zierler <fabian.zierler@a1.net>
2024-11-05 13:28:45 +0000

    fix issue, when the full correlation matrix does not fit into memory

--- GlueballsJulia/scripts/reconstruct_gluon_block.jl
+++ GlueballsJulia/scripts/reconstruct_gluon_block.jl
@@ -3,25 +3,10 @@ using GlueballsJulia
 using ProgressMeter
 using HDF5
 using BenchmarkTools
-
-f = h5open("hdf5/glue_correlators.hdf5","r")["ensemble/0RPpR"]
-
-T = read(f,"NT")
-L = read(f,"NX")
-Nops  = read(f,"Nop")
-Nmeas = 100 #read(f,"Nmeas")
-# Permute indices for better acces 
-corr  = permutedims(f["corr"][:,:,:,1:Nmeas],(4,2,3,1)) # size is (Nmeas Nops, Nops, T/2+1)
-ops   = permutedims(f["ops"][:,:,1:Nmeas],(3,2,1))  # size is (Nmeas, Nops, T) 
-vev   = permutedims(f["vev"][:,1:Nmeas],(2,1))  # size is (Nmeas, Nops)
-@assert size(corr) == (Nmeas, Nops, Nops, T÷2+1)
-@assert size(ops)  == (Nmeas, Nops, T)
-@assert size(vev)  == (Nmeas, Nops)
-
 function reconstruct_corr(ops)
     Nmeas, Nops, T = size(ops)
     corr = zeros(Nmeas,Nops,Nops,T)
-    @showprogress for t1 in 1:T
+    for t1 in 1:T
         for t2 in 1:T
             Δt = mod(t2-t1,T)
             for op1 in 1:Nops
@@ -43,9 +28,20 @@ function test_reconstructiuon(ops,corr)
     return maxdiff
 end
 
-# test reconstruction first
-# TODO: Fix issue with memory
-corr_reconstruct = reconstruct_corr(ops)
-@show hash(corr_reconstruct)
-diff = test_reconstructiuon(ops,corr)
-@assert iszero(diff)
+f = h5open("hdf5/glue_correlators.hdf5","r")["ensemble/0RPpR"]
+T = read(f,"NT")
+L = read(f,"NX")
+Nops  = read(f,"Nop")
+Nmeas = read(f,"Nmeas")
+
+# Compare one configuration at a time to use less memory
+n_batch = 20
+@showprogress for n in Iterators.partition(1:Nmeas, n_batch) 
+    # Permute indices for better memory acces 
+    corr  = permutedims(f["corr"][:,:,:,n],(4,2,3,1)) # size is (Nmeas Nops, Nops, T/2+1)
+    ops   = permutedims(f["ops"][:,:,n],(3,2,1))  # size is (Nmeas, Nops, T) 
+    vev   = permutedims(f["vev"][:,n],(2,1))  # size is (Nmeas, Nops)
+    corr_reconstruct = reconstruct_corr(ops)
+    diff = test_reconstructiuon(ops,corr)
+    @assert iszero(diff)
+end
