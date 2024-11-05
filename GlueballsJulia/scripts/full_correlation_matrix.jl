b87d3913105b0f1225c4df539a2059695518b2dc
Fabian Zierler <fabian.zierler@a1.net>
2024-11-05 14:38:04 +0000

    assemble full gluon correlation matrix  in batches

--- GlueballsJulia/scripts/full_correlation_matrix.jl
+++ GlueballsJulia/scripts/full_correlation_matrix.jl
@@ -4,16 +4,22 @@ using ProgressMeter
 using HDF5
 using BenchmarkTools
 
-function reconstruct_corr(ops)
-    Nmeas, Nops, T = size(ops)
-    corr = zeros(Nmeas,Nops,Nops,T)
+function reconstruct_corr(ops1,ops2)
+    Nmeas1, Nops1, T1 = size(ops1)
+    Nmeas2, Nops2, T2 = size(ops2)
+    @assert T1 == T2
+    @assert Nmeas1 == Nmeas1
+    Nmeas = Nmeas1 
+    T = T1 
+
+    corr = zeros(Nmeas,Nops1,Nops2,T)
     for t1 in 1:T
         for t2 in 1:T
             Δt = mod(t2-t1,T)
-            for op1 in 1:Nops
-                for op2 in 1:Nops
+            for op1 in 1:Nops1
+                for op2 in 1:Nops2
                     for n in 1:Nmeas
-                        corr[n,op2,op1,Δt+1] += (ops[n,op1,t1]*ops[n,op2,t2] + ops[n,op2,t1]*ops[n,op1,t2])/2
+                        corr[n,op1,op2,Δt+1] += (ops1[n,op1,t1]*ops2[n,op2,t2] + ops2[n,op2,t1]*ops1[n,op1,t2])/2
                     end
                 end
             end
@@ -27,20 +33,33 @@ f_ferm = h5open("hdf5/meson_correlators.hdf5","r")
 T = read(f_glue,"NT")
 L = read(f_glue,"NX")
 Nops = read(f_glue,"Nop")
-Nmeas = read(f_glue,"Nmeas")
+Nmeas_glue = read(f_glue,"Nmeas")
 Nmeas_mes = length(read(f_ferm,"M3/plaquette"))
-
-corr_meson = f_ferm["M3/correlation_matrix_g5_singlet"][:,:,:,:]
-corr_glue  = f_glue["corr"][:,:,:,:]
-ops_mesFUN = f_ferm["M3/singlet_loop_g5_FUN"][:,:,:]
-ops_mesAS  = f_ferm["M3/singlet_loop_g5_AS"][:,:,:]
-ops_glue   = f_glue["ops"][:,:,:]
+Nmeas = min(Nmeas_glue,Nmeas_mes) # NOTE: The last 7 configurations for the glueballs are missing
 
 # Construct full correlation matrices in batches to save rAM
 n_batch = 20
-@showprogress for n in Iterators.partition(1:Nmeas, n_batch) 
-    # Permute indices for better memory acces 
-    ops   = permutedims(f["ops"][:,:,n],(3,2,1))  # size is (Nmeas, Nops, T) 
-    #corr_reconstruct = reconstruct_corr(ops)
+for n in Iterators.partition(1:Nmeas, n_batch) 
+    corr_meson = f_ferm["M3/correlation_matrix_g5_singlet"][:,:,n,:]
+    ops_mesFUN = f_ferm["M3/singlet_loop_g5_FUN"][:,n,:]
+    ops_mesAS  = f_ferm["M3/singlet_loop_g5_AS"][:,n,:]
+    ops_glue   = f_glue["ops"][:,:,n]
+    ops_mes    = cat(ops_mesFUN, ops_mesAS,dims=1) # NOTE: The meson operators are ordered as (FUN, AS) in ascending order
+    
+    # Change memory layout for better access patterns
+    ops_mesFUN = permutedims(ops_mesFUN,(2,1,3))
+    ops_mesAS = permutedims(ops_mesAS,(2,1,3))
+    ops_glue = permutedims(ops_glue,(3,2,1))
+    corr_mes = permutedims(corr_meson,(3,1,2,4))
+    ops_mes = permutedims(ops_mes,(2,1,3))
+
+    # Create block-diagonal gluon matrix and meson-gluon cross corelator
+    corr_glue  = reconstruct_corr(ops_glue,ops_glue)
+    corr_cross = reconstruct_corr(ops_glue,ops_mes)
+    corr_cross_transpose = permutedims(corr_cross,(1,3,2,4))
+
+    # assemble full correlation matrix
+    coloumn1 = cat(corr_glue,corr_cross,dims=3)
+    coloumn2 = cat(corr_cross_transpose,corr_mes,dims=3)
+    full = cat(coloumn1,coloumn2,dims=2)
 end
-=#
--- GlueballsJulia/scripts/reconstruct_gluon_block.jl
+++ GlueballsJulia/scripts/reconstruct_gluon_block.jl
@@ -3,16 +3,22 @@ using GlueballsJulia
 using ProgressMeter
 using HDF5
 using BenchmarkTools
-function reconstruct_corr(ops)
-    Nmeas, Nops, T = size(ops)
-    corr = zeros(Nmeas,Nops,Nops,T)
+function reconstruct_corr(ops1,ops2)
+    Nmeas1, Nops1, T1 = size(ops1)
+    Nmeas2, Nops2, T2 = size(ops2)
+    @assert T1 == T2
+    @assert Nmeas1 == Nmeas1
+    Nmeas = Nmeas1 
+    T = T1 
+
+    corr = zeros(Nmeas,Nops1,Nops2,T)
     for t1 in 1:T
         for t2 in 1:T
             Δt = mod(t2-t1,T)
-            for op1 in 1:Nops
-                for op2 in 1:Nops
+            for op1 in 1:Nops1
+                for op2 in 1:Nops2
                     for n in 1:Nmeas
-                        corr[n,op2,op1,Δt+1] += (ops[n,op1,t1]*ops[n,op2,t2] + ops[n,op2,t1]*ops[n,op1,t2])/2
+                        corr[n,op1,op2,Δt+1] += (ops1[n,op1,t1]*ops2[n,op2,t2] + ops2[n,op2,t1]*ops1[n,op1,t2])/2
                     end
                 end
             end
@@ -23,7 +29,7 @@ end
 function test_reconstructiuon(ops,corr)
     Nops, Nmeas, T = size(ops)
     Tmax  = T÷2+1
-    corr_R = reconstruct_corr(ops)
+    corr_R = reconstruct_corr(ops,ops)
     maxdiff = maximum(abs.(corr_R[:,:,:,1:Tmax] - corr))
     return maxdiff
 end
@@ -41,7 +47,7 @@ n_batch = 20
     corr  = permutedims(f["corr"][:,:,:,n],(4,2,3,1)) # size is (Nmeas Nops, Nops, T/2+1)
     ops   = permutedims(f["ops"][:,:,n],(3,2,1))  # size is (Nmeas, Nops, T) 
     vev   = permutedims(f["vev"][:,n],(2,1))  # size is (Nmeas, Nops)
-    corr_reconstruct = reconstruct_corr(ops)
+    corr_reconstruct = reconstruct_corr(ops,ops)
     diff = test_reconstructiuon(ops,corr)
     @assert iszero(diff)
 end
