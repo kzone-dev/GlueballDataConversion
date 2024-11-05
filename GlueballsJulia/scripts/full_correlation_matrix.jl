8d52f3e1dfe01330be981fdc4cadb0245515f27a
Fabian Zierler <fabian.zierler@a1.net>
2024-11-05 17:51:30 +0000

    use function barriers for correlation matrix constructions

--- GlueballsJulia/scripts/full_correlation_matrix.jl
+++ GlueballsJulia/scripts/full_correlation_matrix.jl
@@ -27,61 +27,75 @@ function reconstruct_corr(ops1,ops2)
     end
     return corr
 end
+function write_full_correlation_matrix(fn_glue, fn_mes, fn_full, id_glue, id_ferm, id_ens; n_batch = 200)
+    f_glue = h5open(fn_glue,"r")[id_glue]
+    f_ferm = h5open(fn_mes,"r")[id_ens]
 
+    T = read(f_glue,"NT")
+    L = read(f_glue,"NX")
 
-id_glue = "0RPmR"
-id_ferm = "g5" 
+    # get total number of operators 
+    Nops_glue = read(f_glue,"Nop")
+    Nops_mes  = length(read(f_ferm,"Wuppertal_levels_FUN")) + length(read(f_ferm,"Wuppertal_levels_AS"))
+    Nops = Nops_glue + Nops_mes
 
-id_glue = "0RPpR"
-id_ferm = "id" 
+    # check that the number of measurements matches
+    Nmeas_glue = read(f_glue,"Nmeas")
+    Nmeas_mes = length(read(f_ferm,"plaquette"))
+    Nmeas = min(Nmeas_glue,Nmeas_mes) # NOTE: The last 7 configurations for the glueballs are missing
 
-f_glue = h5open("hdf5/glue_correlators.hdf5","r")["ensemble/$id_glue"]
-f_ferm = h5open("hdf5/meson_correlators.hdf5","r")["M3"]
-T = read(f_glue,"NT")
-L = read(f_glue,"NX")
-Nops_glue = read(f_glue,"Nop")
-Nops_mes  = length(read(f_ferm,"Wuppertal_levels_FUN")) + length(read(f_ferm,"Wuppertal_levels_AS"))
-Nmeas_glue = read(f_glue,"Nmeas")
-Nmeas_mes = length(read(f_ferm,"plaquette"))
-configs = read(f_ferm,"configurations")
-Nmeas = min(Nmeas_glue,Nmeas_mes) # NOTE: The last 7 configurations for the glueballs are missing
-Nops = Nops_glue + Nops_mes
+    # Create a hdf5 dataset for the full correlation matrix without ever loading it into memory
+    # Only, later we will write to the file in batches.
+    f = h5open(fn_full,"w")
+    create_dataset(f, "full_correlation_matrix", Float64, (Nmeas,Nops,Nops,T))
 
-# Create a hdf5 dataset for the full correlation matrix without ever loading it into memory
-# Only, later we will write to the file in batches.
-f = h5open("hdf5/correlation_matrix_$id_ferm.hdf5","w")
-create_dataset(f, "full_correlation_matrix_$id_ferm", Float64, (Nmeas,Nops,Nops,T))
+    # Construct full correlation matrices in batches to save rAM
+    @showprogress for (i,n) in enumerate(Iterators.partition(1:Nmeas, n_batch)) 
+        corr_meson = f_ferm["correlation_matrix_$(id_ferm)_singlet"][:,:,n,:]
+        ops_mesFUN = f_ferm["singlet_loop_$(id_ferm)_FUN"][:,n,:]
+        ops_mesAS  = f_ferm["singlet_loop_$(id_ferm)_AS"][:,n,:]
+        ops_glue   = f_glue["ops"][:,:,n]
+        ops_mes    = cat(ops_mesFUN, ops_mesAS,dims=1) # NOTE: The meson operators are ordered as (FUN, AS) in ascending order
+        
+        # Change memory layout for better access patterns
+        ops_mesFUN = permutedims(ops_mesFUN,(2,1,3))
+        ops_mesAS = permutedims(ops_mesAS,(2,1,3))
+        ops_glue = permutedims(ops_glue,(3,2,1))
+        corr_mes = permutedims(corr_meson,(3,1,2,4))
+        ops_mes = permutedims(ops_mes,(2,1,3))
 
-# Construct full correlation matrices in batches to save rAM
-n_batch = 100
-@showprogress for (i,n) in enumerate(Iterators.partition(1:Nmeas, n_batch)) 
-    corr_meson = f_ferm["correlation_matrix_$(id_ferm)_singlet"][:,:,n,:]
-    ops_mesFUN = f_ferm["singlet_loop_$(id_ferm)_FUN"][:,n,:]
-    ops_mesAS  = f_ferm["singlet_loop_$(id_ferm)_AS"][:,n,:]
-    ops_glue   = f_glue["ops"][:,:,n]
-    ops_mes    = cat(ops_mesFUN, ops_mesAS,dims=1) # NOTE: The meson operators are ordered as (FUN, AS) in ascending order
-    
-    # Change memory layout for better access patterns
-    ops_mesFUN = permutedims(ops_mesFUN,(2,1,3))
-    ops_mesAS = permutedims(ops_mesAS,(2,1,3))
-    ops_glue = permutedims(ops_glue,(3,2,1))
-    corr_mes = permutedims(corr_meson,(3,1,2,4))
-    ops_mes = permutedims(ops_mes,(2,1,3))
+        # Create block-diagonal gluon matrix and meson-gluon cross corelator
+        corr_glue  = reconstruct_corr(ops_glue,ops_glue)
+        corr_cross = reconstruct_corr(ops_glue,ops_mes)
+        corr_cross_transpose = permutedims(corr_cross,(1,3,2,4))
 
-    # Create block-diagonal gluon matrix and meson-gluon cross corelator
-    corr_glue  = reconstruct_corr(ops_glue,ops_glue)
-    corr_cross = reconstruct_corr(ops_glue,ops_mes)
-    corr_cross_transpose = permutedims(corr_cross,(1,3,2,4))
-
-    # assemble full correlation matrix
-    coloumn1 = cat(corr_glue,corr_cross,dims=3)
-    coloumn2 = cat(corr_cross_transpose,corr_mes,dims=3)
-    full = cat(coloumn1,coloumn2,dims=2)
-    
-    f["full_correlation_matrix_$(id_ferm)"][n,:,:,:] = full
+        # assemble full correlation matrix
+        coloumn1 = cat(corr_glue,corr_cross,dims=3)
+        coloumn2 = cat(corr_cross_transpose,corr_mes,dims=3)
+        full = cat(coloumn1,coloumn2,dims=2)
+        
+        f["full_correlation_matrix"][n,:,:,:] = full
+    end
+    f["T"] = T
+    f["L"] = L
+    f["Nmeas"] = Nmeas
+    f["Nops"] = Nops
+    close(f)
 end
-f["T"] = T
-f["L"] = L
-f["Nmeas"] = Nmeas
-f["Nops"] = Nops
-close(f)
+
+
+id_glue = "ensemble/0RPmR"
+id_ferm = "g5" 
+id_ens  = "M3"
+fn_glue = "hdf5/glue_correlators.hdf5"
+fn_mes  = "hdf5/meson_correlators.hdf5"
+fn_full = "hdf5/correlation_matrix_$id_ferm.hdf5"
+write_full_correlation_matrix(fn_glue, fn_mes, fn_full, id_glue, id_ferm, id_ens)
+
+id_glue = "ensemble/0RPpR"
+id_ferm = "id" 
+id_ens  = "M3"
+fn_glue = "hdf5/glue_correlators.hdf5"
+fn_mes  = "hdf5/meson_correlators.hdf5"
+fn_full = "hdf5/correlation_matrix_$id_ferm.hdf5"
+write_full_correlation_matrix(fn_glue, fn_mes, fn_full, id_glue, id_ferm, id_ens)
