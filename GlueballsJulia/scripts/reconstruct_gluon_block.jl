5c3c8cd2eb987f5f9b073555aa848ae47440b087
Fabian Zierler <fabian.zierler@a1.net>
2024-11-05 11:46:21 +0000

    new memory accfess pattern

--- GlueballsJulia/scripts/reconstruct_gluon_block.jl
+++ GlueballsJulia/scripts/reconstruct_gluon_block.jl
@@ -8,38 +8,39 @@ f = h5open("hdf5/glue_correlators.hdf5","r")["ensemble/0RPpR"]
 T = read(f,"NT")
 L = read(f,"NX")
 Nops  = read(f,"Nop")
-Nmeas = read(f,"Nmeas")
-#corr  = read(f,"corr") # size is ( T/2+1, Nops, Nops, Nmeas)
-ops   = read(f,"ops")  # size is (T, Nops, Nmeas) 
-vev   = read(f,"vev")  # size is (Nops, Nmeas)
-#@assert size(corr) == (T÷2+1, Nops, Nops, Nmeas)
-@assert size(ops)  == (T, Nops, Nmeas)
-@assert size(vev)  == (Nops, Nmeas)
+Nmeas = 10 # read(f,"Nmeas")
+# Permute indices for better acces 
+corr  = permutedims(f["corr"][:,:,:,1:Nmeas],(4,2,3,1)) # size is (Nmeas Nops, Nops, T/2+1)
+ops   = permutedims(f["ops"][:,:,1:Nmeas],(3,2,1))  # size is (Nmeas, Nops, T) 
+vev   = permutedims(f["vev"][:,1:Nmeas],(2,1))  # size is (Nmeas, Nops)
+@assert size(corr) == (Nmeas, Nops, Nops, T÷2+1)
+@assert size(ops)  == (Nmeas, Nops, T)
+@assert size(vev)  == (Nmeas, Nops)
 
 function reconstruct_corr(ops)
-    T, Nops, Nmeas = size(ops)
-    corr = zeros(T,Nops,Nops,Nmeas)
-    @showprogress for t1 in 1:T
+    Nmeas, Nops, T = size(ops)
+    corr = zeros(Nmeas,Nops,Nops,T)
+    for t1 in 1:T
         for t2 in 1:T
             Δt = mod(t2-t1,T)
-            for n in 1:Nmeas, op1 in 1:Nops, op2 in 1:Nops
-                corr[Δt+1,op1,op2,n] += (ops[t1,op1,n]*ops[t2,op2,n] + ops[t1,op2,n]*ops[t2,op1,n])/2
+            for op1 in 1:Nops, op2 in 1:Nops, n in 1:Nmeas
+                corr[n,op2,op1,Δt+1] += (ops[n,op1,t1]*ops[n,op2,t2] + ops[n,op2,t1]*ops[n,op1,t2])/2
             end
         end
     end
     return corr
 end
-function test_reconstructiuon(ops,corr0)
-    T, Nops, Nmeas = size(ops)
+function test_reconstructiuon(ops,corr)
+    Nops, Nmeas, T = size(ops)
     Tmax  = T÷2+1
-    corr1 = reconstruct_corr(ops)
-    maxdiff = maximum(abs.(corr0 - corr1[1:Tmax,:,:,:]))
+    corr_R = reconstruct_corr(ops)
+    maxdiff = maximum(abs.(corr_R[:,:,:,1:Tmax] - corr))
     return maxdiff
 end
 
 # test reconstruction first
 # TODO: Fix issue with memory
 corr_reconstruct = reconstruct_corr(ops)
-#diff = test_reconstructiuon(ops,corr)
-#@assert iszero(diff)
-#corr_reconstruct = reconstruct_corr(ops)
+@show hash(corr_reconstruct)
+diff = test_reconstructiuon(ops,corr)
+@assert iszero(diff)
