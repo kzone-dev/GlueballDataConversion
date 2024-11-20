4774e0c15b4178b447d6abc4a2b990255d87d77e
Fabian Zierler <fabian.zierler@a1.net>
2024-11-20 07:40:32 +0000

    save vev for jackknife resamples

--- GlueballsJulia/scripts/jackknife_samples.jl
+++ GlueballsJulia/scripts/jackknife_samples.jl
@@ -5,19 +5,18 @@ using HDF5
 using BenchmarkTools
 using Statistics
 
-function _copy_lattice_parameters(outfile,infile,ensemble;group="")
-    file = h5open(infile)[ensemble]
-    entries = filter(!contains(r"(correlation_matrix|singlet_loop)") ,keys(file))
+function _copy_lattice_parameters(outfile,infile;group="")
+    file = h5open(infile)
+    entries = filter(!contains(r"(correlation_matrix|singlet_loop|vev)") ,keys(file))
+    @show entries
     for entry in entries
         label = joinpath(group,entry)
         h5write(outfile,label,read(file,entry))
     end
 end
-function resample_full_correlator(file_in, file_out; n_batch = 100)
-    _copy_lattice_parameters(file_out,file_in,"")
+function resample_full_correlator(file_in, file_out; n_batch = 100, save_vev = false)
     f1 = h5open(file_in,"r")
     f2 = h5open(file_out,"w")
-
     Nmeas = read(f1,"Nmeas")
     Nops  = read(f1,"Nops")
     T     = read(f1,"T")
@@ -26,14 +25,22 @@ function resample_full_correlator(file_in, file_out; n_batch = 100)
     # Construct full correlation matrices in batches to save RAM
     resample_corr = zeros(n_batch,Nops,Nops,T)
     resample_sum  = zeros(Nops,Nops,T)
-
+    if save_vev
+        vev = f1["full_vev"]
+        resample_vev = zeros(n_batch,Nops)
+        resample_vev_sum = zeros(Nops)
+    end
     # Create a hdf5 dataset for the full correlation matrix without ever loading it into memory
     # Only, later we will write to the file in batches.
     create_dataset(f2, "correlation_matrix_resample", Float64, (Nmeas,Nops,Nops,T))
+    create_dataset(f2, "vev_resample", Float64, (Nmeas,Nops))
 
     # sum over all configurations, then subtract the one configuration each to get a jackknife resample
     @showprogress for n in Iterators.partition(1:Nmeas, n_batch)
         resample_sum .= resample_sum .+ dropdims(sum(corr[n,:,:,:],dims=1),dims=1) 
+        if save_vev
+            resample_vev_sum .= resample_vev_sum .+ dropdims(sum( vev[n,:],dims=1),dims=1) 
+        end
     end
 
     # remove one configuration each and save the resulting resample to the hdf5 file
@@ -43,16 +50,24 @@ function resample_full_correlator(file_in, file_out; n_batch = 100)
         @inbounds for t in 1:T, ind3 in 1:Nops, ind2 in 1:Nops, ind1 in eachindex(n)
             resample_corr[ind1,ind2,ind3,t] = resample_sum[ind2,ind3,t] - batch[ind1,ind2,ind3,t]
         end
+        if save_vev
+            batch_vev = vev[n,:]
+            @inbounds for ind2 in 1:Nops, ind1 in eachindex(n)
+                resample_vev[ind1,ind2] = resample_vev_sum[ind2] - batch_vev[ind1,ind2]
+            end
+            f2["vev_resample"][n,:] = resample_vev[eachindex(n),:]
+        end
         f2["correlation_matrix_resample"][n,:,:,:] = resample_corr[eachindex(n),:,:,:]
     end
     close(f1)
     close(f2)
+    _copy_lattice_parameters(file_out,file_in)
 end
 
-file_in  ="hdf5/correlation_matrix_g5.hdf5"
-file_out ="hdf5/correlation_matrix_g5_resamples.hdf5"    
-resample_full_correlator(file_in, file_out)
+#file_in  ="hdf5/correlation_matrix_g5.hdf5"
+#file_out ="hdf5/correlation_matrix_g5_resamples.hdf5"    
+#resample_full_correlator(file_in, file_out)
 
 file_in  ="hdf5/correlation_matrix_id.hdf5"
 file_out ="hdf5/correlation_matrix_id_resamples.hdf5"    
-resample_full_correlator(file_in, file_out)
+resample_full_correlator(file_in, file_out, save_vev=true)
