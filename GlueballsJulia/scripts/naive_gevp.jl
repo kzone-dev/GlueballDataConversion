9e8c1b264d630ac7563407b1b777065f4b6d0fc2
Fabian Zierler <fabian.zierler@a1.net>
2025-01-15 15:03:49 +0000

    put gevp analysis of resamples in a function call

--- GlueballsJulia/scripts/naive_gevp.jl
+++ GlueballsJulia/scripts/naive_gevp.jl
@@ -11,29 +11,33 @@ function _copy_lattice_parameters(outfile,infile;group="")
     end
 end
 
-file_in  = "./hdf5/correlation_matrix_g5_resamples.hdf5"
-file_out = "./hdf5/gevp_results_g5_resamples.hdf5"
-_copy_lattice_parameters(file_out,file_in)
+function gevp_on_resamples(file_in, file_out; n_batch, t0, imag_thresh)
+    _copy_lattice_parameters(file_out,file_in)
 
-fid  = h5open(file_in)
-resamples = fid["correlation_matrix_resample"]
+    fid  = h5open(file_in)
+    resamples = fid["correlation_matrix_resample"]
 
-Nmeas, Nops, Nops, T = size(resamples)
-eigvals = zeros(Nmeas, Nops, T)
+    Nmeas, Nops, Nops, T = size(resamples)
 
-n_batch = 10
-t0 = 1
-imag_thresh = 1E-11
+    f = h5open(file_out,"w")
+    create_dataset(f, "eigenvalues", Float64, (Nops,Nmeas,T))
+    create_dataset(f, "eigenvectors", ComplexF64, (Nops,Nops,Nmeas,T))
+    h5write(file_out,"t0",t0)
 
-f = h5open(file_out,"w")
-create_dataset(f, "eigenvalues", Float64, (Nops,Nmeas,T))
-create_dataset(f, "eigenvectors", ComplexF64, (Nops,Nops,Nmeas,T))
-h5write(file_out,"t0",t0)
-
-@showprogress for n in Iterators.partition(1:Nmeas, n_batch)
-    # change data layout of data to match the required call to the gevp function
-    batch = permutedims(resamples[n,:,:,:],(3,2,1,4))
-    vals, vecs = eigenvalues_eigenvectors_from_samples(batch; t0, imag_thresh)
-    f["eigenvalues"][:,n,:] = vals
-    f["eigenvectors"][:,:,n,:] = vecs
+    @showprogress for n in Iterators.partition(1:Nmeas, n_batch)
+        # change data layout of data to match the required call to the gevp function
+        batch = permutedims(resamples[n,:,:,:],(3,2,1,4))
+        vals, vecs = eigenvalues_eigenvectors_from_samples(batch; t0, imag_thresh)
+        f["eigenvalues"][:,n,:] = vals
+        f["eigenvectors"][:,:,n,:] = vecs
+    end
+    close(f)
+    close(fid)
 end
+
+using LinearAlgebra
+BLAS.set_num_threads(1) 
+
+file_in  = "./hdf5/correlation_matrix_g5_resamples.hdf5"
+file_out = "./hdf5/gevp_results_g5_resamples_t0_3.hdf5"
+gevp_on_resamples(file_in, file_out; n_batch = 50, t0 = 1, imag_thresh = 1E-11)
