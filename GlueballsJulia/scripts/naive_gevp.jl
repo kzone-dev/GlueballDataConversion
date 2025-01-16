b12b0d7b6ecc50f608c2ee6d8c6f40c31b2e6b5d
Fabian Zierler <fabian.zierler@a1.net>
2025-01-16 09:26:50 +0000

    apply jackknife to gevp results

--- GlueballsJulia/scripts/naive_gevp.jl
+++ GlueballsJulia/scripts/naive_gevp.jl
@@ -1,14 +1,16 @@
 using Pkg; Pkg.activate("GlueballsJulia")
+Pkg.instantiate()
 using HDF5
 using ProgressMeter
 using LatticeUtils
 using LinearAlgebra
+using Statistics
 BLAS.set_num_threads(1)
 @show Threads.nthreads()
 
 function _copy_lattice_parameters(outfile,infile;group="")
     file = h5open(infile)
-    entries = filter(!contains(r"(correlation_matrix|singlet_loop|vev)") ,keys(file))
+    entries = filter(!contains(r"(correlation_matrix|eigen|singlet_loop|vev)") ,keys(file))
     for entry in entries
         label = joinpath(group,entry)
         h5write(outfile,label,read(file,entry))
@@ -26,7 +28,7 @@ function eigenvalues_eigenvectors_from_samples_threaded(batch; t0, imag_thresh)
     vecs = cat(last.(res)...,dims=3)
     return vals, vecs
 end
-function gevp_on_resamples(file_in, file_out; n_batch, t0, imag_thresh)
+function gevp_on_resamples(file_in, file_out; n_batch, t0, imag_thresh=+Inf)
     _copy_lattice_parameters(file_out,file_in)
 
     fid  = h5open(file_in)
@@ -39,18 +41,57 @@ function gevp_on_resamples(file_in, file_out; n_batch, t0, imag_thresh)
     create_dataset(f, "eigenvectors", ComplexF64, (Nops,Nops,Nmeas,T))
     h5write(file_out,"t0",t0)
 
-    @showprogress for n in Iterators.partition(1:Nmeas, n_batch)
+    @showprogress desc="perform GEVP" for n in Iterators.partition(1:Nmeas, n_batch)
         # change data layout of data to match the required call to the gevp function
         batch = permutedims(resamples[n,:,:,:],(3,2,1,4))
         #vals0, vecs0 = eigenvalues_eigenvectors_from_samples(batch; t0, imag_thresh)
         vals , vecs  = eigenvalues_eigenvectors_from_samples_threaded(batch; t0, imag_thresh)
-        f["eigenvalues"][:,n,:] = vals
+        f["eigenvalues"][:,n,:]    = vals
         f["eigenvectors"][:,:,n,:] = vecs
     end
     close(f)
     close(fid)
 end
+function apply_jackknife_gevp(file_in, file_out; n_batch)
+    _copy_lattice_parameters(file_out,file_in)
 
+    fid  = h5open(file_in)
+    vals_samples = fid["eigenvalues"]
+    vecs_samples = fid["eigenvectors"]
+    Nops,Nmeas,T = size(vals_samples)
+
+    vals  = zeros(Float64, (Nops,1,T))
+    Δvals = zeros(Float64, (Nops,1,T))
+    vecs  = zeros(ComplexF64, (Nops,Nops,1,T))
+    Δvecs = zeros(ComplexF64, (Nops,Nops,1,T))
+
+    @showprogress desc="jackknife mean" for n in Iterators.partition(1:Nmeas, n_batch)
+        vals_batch = vals_samples[:,n,:]
+        vecs_batch = vecs_samples[:,:,n,:]
+        vals += sum(vals_batch,dims=2)/Nmeas
+        vecs += sum(vecs_batch,dims=3)/Nmeas
+    end
+    @showprogress desc="jackknife error" for n in Iterators.partition(1:Nmeas, n_batch)
+        vals_batch = vals_samples[:,n,:]
+        vecs_batch = vecs_samples[:,:,n,:]
+        Δvals += sqrt(Nmeas-1)*std(vals_batch,mean=vals,dims=2,corrected=false)
+        Δvecs += sqrt(Nmeas-1)*std(vecs_batch,mean=vecs,dims=3,corrected=false)
+    end
+    close(fid)
+
+    h5write(file_out,"eigenvalues"       , dropdims(vals,dims=2))
+    h5write(file_out,"eigenvectors"      , dropdims(vecs,dims=3))
+    h5write(file_out,"Delta_eigenvalues" , dropdims(Δvals,dims=2))
+    h5write(file_out,"Delta_eigenvectors", dropdims(Δvecs,dims=3))
+end
+
+file_tmp = "./hdf5/tmp.hdf5"
 file_in  = "./hdf5/correlation_matrix_g5_resamples.hdf5"
-file_out = "./hdf5/gevp_results_g5_resamples_parallel.hdf5"
-gevp_on_resamples(file_in, file_out; n_batch = Threads.nthreads()*5, t0 = 1, imag_thresh = +Inf)
+file_out = "./hdf5/gevp_results_g5_t0_1.hdf5"
+
+t0 = 1
+n_batch = Threads.nthreads()*5
+
+gevp_on_resamples(file_in, file_tmp; n_batch, t0 )
+apply_jackknife_gevp(file_tmp, file_out; n_batch )
+rm(file_tmp)
