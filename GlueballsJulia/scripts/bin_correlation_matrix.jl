a2654907148867d1787bb2b9e34a4793cdf07195
kzone-dev <nmrbrito2000@gmail.com>
2026-01-26 09:34:43 +0000

    binning script also removes bad vevs. MixedRepSinglets repo now include all smeared ops for M4 ensemble

--- GlueballsJulia/scripts/bin_correlation_matrix.jl
+++ GlueballsJulia/scripts/bin_correlation_matrix.jl
@@ -1,4 +1,4 @@
-using Pkg; Pkg.resolve(); Pkg.instantiate(); Pkg.update(); Pkg.precompile(); Pkg.activate(".")
+using Pkg; Pkg.activate(".")
 using HDF5
 using Statistics
 using ProgressMeter
@@ -10,31 +10,61 @@ function _copy_lattice_parameters(outfile,infile;group="")
         h5write(outfile,label,read(file,entry))
     end
 end
-function bin_correlation_marix(file_in,file_out; binsize = 8, batchsize = 50, include_vev = false)
+function filter_bad_vevs(vev_data, meson_ops_array; config_number = 2507)
+    # In the M3 ensemble, after config 2507 there are measurements
+    # with bad vev values for the smeared operators. We filter them out here.
+
+    reference_vev_values = dropdims(mean(vev_data[1:config_number,meson_ops_array],dims=1),dims=1)
+    reference_vev_values_std = dropdims(std(vev_data[1:config_number,meson_ops_array],dims=1),dims=1)
+
+    Nconfs = size(vev_data,1)
+    Nops = size(vev_data,2)
+    bad_indices = Bool[false for i in 1:Nconfs]
+    
+    for iconf in 1:Nconfs
+        for (i, jop) in enumerate(meson_ops_array)
+            relative_error = abs(vev_data[iconf,jop] - reference_vev_values[i]) / reference_vev_values_std[i]
+            if relative_error> 5
+                bad_indices[iconf] = true
+                #println("Excluding configuration ", iconf, " due to operator ", jop,
+                #        ": vev = ", vev_data[iconf,jop],
+                #        ", reference vev = ", reference_vev_values[i],
+                #        ", relative error= ", relative_error)
+                break
+            end
+        end
+    end
+    return bad_indices
+end
+function bin_correlation_marix(file_in,file_out; binsize = 8, batchsize = 50, include_vev = false, filter_vev = false)
     fid   = h5open(file_in)
     Nmeas = read(fid,"Nmeas")
     Nops  = read(fid,"Nops")
     T     = read(fid,"T")
 
-    iterator    = Iterators.partition(1:Nmeas, batchsize*binsize) 
-    nbins       = length(Iterators.partition(1:Nmeas, binsize))
+    filtered_Nmeas = 1:Nmeas
+    if filter_vev
+        bad_indices= filter_bad_vevs(fid["full_vev"], 161:169; config_number = 2507)
+        filtered_Nmeas = collect(1:Nmeas)[.!bad_indices]
+    end 
+    
+    iterator    = Iterators.partition(filtered_Nmeas, batchsize*binsize) 
+    nbins       = length(Iterators.partition(filtered_Nmeas, binsize))
+
     corr_binned = zeros(nbins,Nops,Nops,T)
     if include_vev
         vev_binned  = zeros(nbins, Nops)  # only if include_vev = true
     end
 
-    println("Size of loaded correlation matrix chunk: ", size(fid["full_correlation_matrix"]))
-    if include_vev
-        println("Size of loaded vev chunk: ", size(fid["full_vev"]))
-    end
+    corr_data = read(fid,"full_correlation_matrix")
+    vev_data = include_vev ? read(fid,"full_vev") : nothing
 
     p = Progress(nbins)
     ind = 0
     for n in iterator
-        corr_tmp = fid["full_correlation_matrix"][n,:,:,:]
+        corr_tmp = corr_data[n,:,:,:]
         if include_vev
-            vev_tmp = fid["full_vev"][n,:]
-            println("binning a chunk of vev size: ", size(vev_tmp))
+            vev_tmp = vev_data[n,:]
         end
         for m in Iterators.partition(1:length(n),binsize)
             ind += 1
@@ -53,13 +83,13 @@ function bin_correlation_marix(file_in,file_out; binsize = 8, batchsize = 50, in
     _copy_lattice_parameters(file_out,file_in;group="")
 end
 
-file_in  = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M3correlation_matrix_id.hdf5"
-file_out = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M3correlation_matrix_id_bin16.hdf5"
-bin_correlation_marix(file_in,file_out; binsize = 16, batchsize = 50, include_vev = true)
+#file_in  = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M3correlation_matrix_id.hdf5"
+#file_out = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M3correlation_matrix_id_bin16.hdf5"
+#bin_correlation_marix(file_in,file_out; binsize = 16, batchsize = 50, include_vev = true, filter_vev = true)
 
-file_in  = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M3correlation_matrix_g5.hdf5"
-file_out = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M3correlation_matrix_g5_bin16.hdf5"
-bin_correlation_marix(file_in,file_out; binsize = 16, batchsize = 50, include_vev = false)
+#file_in  = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M3correlation_matrix_g5.hdf5"
+#file_out = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M3correlation_matrix_g5_bin16.hdf5"
+#bin_correlation_marix(file_in,file_out; binsize = 16, batchsize = 50, include_vev = false)
 
 file_in  = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M4correlation_matrix_id.hdf5"
 file_out = "/users/nrebelobrito/flavour_singlet_and_glueball_mixing_sp4/data/final_correlation_matrices/M4correlation_matrix_id_bin16.hdf5"
